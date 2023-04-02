928eb0f79f2895d52df319cf6c0c8d6e70163f93
gorge <1660047480@qq.com>
2023-04-02 23:25:29 +0800

    RocketMq消费者设置随着程序的启动而执行

--- src/main/java/com/example/webtest/ControllerTest/RocketMqTest/api/ConsumerClient.java
+++ src/main/java/com/example/webtest/ControllerTest/RocketMqTest/api/ConsumerClient.java
@@ -2,7 +2,11 @@ package com.example.webtest.ControllerTest.RocketMqTest.api;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 /**
@@ -10,11 +14,15 @@ import javax.annotation.Resource;
  * @Version 1.0
  * @Date 2023/2/18 13:34
  **/
+@Component
 public class ConsumerClient {
     @Resource
     MqConfig mqConfig;
     @Resource
     ConsumerMessageListener consumerMessageListener;
+    @Bean
+//    @PostConstruct 都可以设置下面的方法随着程序的启动而执行
+//    @EventListener
     public void consumerMessage() throws MQClientException {
         // 创建接收消息的对象
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(mqConfig.getGroupName());
